6e025b45c45f7c6c003c4639552a6441f8c05d5c
serj20051999 <serj.ohanyan.y@tumo.org>
2020-01-18 16:49:20 +0400

    commit

---
+++ script.js
@@ -0,0 +1,126 @@
+let matrix = [];
+let side = 10;
+let grassArr = [];
+let grassEaterArr = [];
+let predatorArr = [];
+let changerArr = [];
+let monsterArr = [];
+// let img;
+
+// function preload() {
+//   img = loadImage('assets/grapefruit-slice-332-332.jpg');
+// }
+
+function setup() {
+    matrixGenerator(80, 500, 50, 30,150,50);
+    createCanvas(matrix[0].length * side, matrix.length * side);
+    background('grey');
+    noStroke();
+    frameRate(5);
+    // image(img, 0, 0);
+
+
+    function matrixGenerator(matrixSize, grassCount, grassEaterCount, predatorCount,changerCount,monsterCount) {
+        for (let index = 0; index < matrixSize; index++) {
+            matrix[index] = [];
+            for (let i = 0; i < matrixSize; i++) {
+                matrix[index][i] = 0;
+            }
+        }
+        for (let index = 0; index < grassCount; index++) {
+            let x = Math.floor(random(0, matrixSize));
+            let y = Math.floor(random(0, matrixSize));
+            matrix[y][x] = 1;
+        }
+        for (let index = 0; index < grassEaterCount; index++) {
+            let x = Math.floor(random(0, matrixSize));
+            let y = Math.floor(random(0, matrixSize));
+            matrix[y][x] = 2;
+        }
+        for (let index = 0; index < predatorCount; index++) {
+            let x = Math.floor(random(0, matrixSize));
+            let y = Math.floor(random(0, matrixSize));
+            matrix[y][x] = 3;
+        }
+        for (let index = 0; index < changerCount; index++) {
+            let x = Math.floor(random(0, matrixSize));
+            let y = Math.floor(random(0, matrixSize));
+            matrix[y][x] = 4;
+        }
+        for (let index = 0; index < monsterCount; index++) {
+            let x = Math.floor(random(0, matrixSize));
+            let y = Math.floor(random(0, matrixSize));
+            matrix[y][x] = 5;
+        }
+    }
+
+    for (let y = 0; y < matrix.length; y++) {
+        for (let x = 0; x < matrix[y].length; x++) {
+            if (matrix[y][x] == 1) {
+                let grass = new Grass(x, y);
+                grassArr.push(grass);
+            }
+            else if (matrix[y][x] == 2) {
+                let grassEater = new GrassEater(x, y);
+                grassEaterArr.push(grassEater);
+            }
+            else if (matrix[y][x] == 3) {
+                let predator = new Predator(x, y);
+                predatorArr.push(predator);
+            }
+            else if (matrix[y][x] == 4) {
+                let changer = new Change(x, y);
+                changerArr.push(changer);
+            }
+            else if (matrix[y][x] == 5) {
+                let monster = new Monster(x, y);
+                monsterArr.push(monster);
+            }
+        }
+    }
+
+}
+
+function draw() {
+
+    for (let y = 0; y < matrix.length; y++) {
+        const element = matrix[y];
+        for (let x = 0; x < element.length; x++) {
+
+            if (matrix[y][x] == 1) {
+                fill('green')
+            }
+            else if (matrix[y][x] == 2) {
+                fill('yellow')
+            }
+            else if (matrix[y][x] == 3) {
+                fill('red')
+            }
+            else if (matrix[y][x] == 4) {
+                fill('cyan')
+            }
+            else if (matrix[y][x] == 5) {
+                fill('black')
+            }
+            else {
+                fill('grey')
+            }
+            rect(x * side, y * side, side, side)
+        }
+    }
+    for (let index = 0; index < grassArr.length; index++) {
+        grassArr[index].mul();
+    }
+    for (let index = 0; index < grassEaterArr.length; index++) {
+        grassEaterArr[index].eat();
+    }
+    for (let index = 0; index < predatorArr.length; index++) {
+        predatorArr[index].eat();
+    }
+    for (let index = 0; index < changerArr.length; index++) {
+        changerArr[index].change();
+    }
+    for (let index = 0; index < monsterArr.length; index++) {
+        monsterArr[index].eat();
+    }
+}
